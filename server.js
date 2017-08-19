b787e0423d1d0686984cabc68314f80da61fe311
Rani11 <30871004+Rani11@users.noreply.github.com>
2017-08-19 12:36:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
     title: 'Article One | Jhansi Rani',
     heading: 'Article One',
     date: 'August 19 2017',
